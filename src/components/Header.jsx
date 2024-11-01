76a40400560833723020d785cd024bca8eefdba0
demmy-tee <danielayo654@gmail.com>
2024-11-02 00:02:12 +0100

    create header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,42 @@
-import React from 'react'
+import { FaSearch } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
     <header className=' bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <Link  to='/'>
            <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
         <span className='text-slate-400'>Elsela</span>
         <span className='text-slate-950'>Inc</span>
       </h1>
-      <form >
-          <input type="text" placeholder='Search...' />
-      </form>
+      </Link>
+      <form className=' bg-slate-100 p-3 rounded-lg flex items-center justify-between'>
+          <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' />
+          <FaSearch className='text-slate-600 '/>
+        </form>
+
+        <ul className='flex gap-4'>
+          <Link to='/'>
+          <li className='hidden sm:inline
+           text-slate-700
+           border-b-0 hover:border-b-[1.9px]
+            hover:border-slate-700
+            transition duration-[2s]
+            ease-in-out'>Home </li>
+          </Link>
+          <Link to='/about'>
+          <li  className='hidden sm:inline
+           text-slate-700
+           border-b-0 hover:border-b-[1.9px] hover:border-slate-700 transition duration-[2s] ease-in-out'>About</li>
+          </Link>
+          <Link to='sign-in'>
+          <li  className='sm:inline
+           text-slate-700
+           border-b-0 hover:border-b-[1.9px] hover:border-slate-700 transition duration-[2s] ease-in-out'> Sign in</li>
+            </Link>
+        </ul>
+        
    </div>
     </header>
   )
