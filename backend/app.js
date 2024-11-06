60adbb740b0d8b231b8fa3cee2bdf704625643f6
demmy-tee <danielayo654@gmail.com>
2024-11-06 06:59:44 +0100

    database was connected to

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config()
+
+const dbURI = process.env.URI
+
+mongoose.connect(dbURI)
+  .then(() => console.log('connected to mongoDB database'))
+  .catch(err=> console.error(err)
+  )
+
+
 
 
 
