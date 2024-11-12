da85ffe9eb06079deda8c77b61727402c6cf5914
demmy-tee <danielayo654@gmail.com>
2024-11-12 21:51:47 +0100

    added functionality for the signup page

--- documentation.js
+++ documentation.js
@@ -4,6 +4,10 @@ Documentation on how the mern-stack app was built
 first install react and tailwind,
 install react-router-dom
 instal react-icons from npm
-
+// dynamic object assignment
+//useNavigate from react-router-dom
+useEffect is used for get request not post request
+// redux query
+//
 
 */
